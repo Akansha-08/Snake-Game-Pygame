c90664124d69d4021185a658d797e569f3a65573
Akansha <30.akansha.s@gmail.com>
2026-03-06 15:55:10 +0530

    Improved code readability and optimized food generation logic

--- snake.py
+++ snake.py
@@ -1,5 +1,4 @@
 import pygame
-from pygame.locals import *
 import time
 import random
 
@@ -8,7 +7,7 @@ red = (255,0,0)
 blue = (51,153,255)
 grey = (192,192,192)
 green = (51,102,0)
-yellow = (0,255,255)
+food_color = (255,0,0)
 
 win_width = 600
 win_height = 400
@@ -41,8 +40,8 @@ def game_loop():
     gameOver = False
     gameClose = False
 
-    x1 = win_width/2
-    y1 = win_height/2
+    x1 = win_width // 2
+    y1 = win_height // 2
 
     x1_change = 0
     y1_change = 0
@@ -50,8 +49,8 @@ def game_loop():
     snake_length_list =[]
     snake_length = 1
 
-    foodx = round(random.randrange(0, win_width - snake)/10.0)*10.0
-    foody = round(random.randrange(0, win_height - snake)/10.0)*10.0
+    foodx = random.randrange(0, win_width - snake, 10)
+    foody = random.randrange(0, win_height - snake, 10)
 
     while not gameOver:
 
@@ -71,16 +70,16 @@ def game_loop():
 
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == K_LEFT:
+                if event.key == pygame.K_LEFT:
                     x1_change = -snake
                     y1_change = 0
-                if event.key == K_RIGHT:
+                if event.key == pygame.K_RIGHT:
                     x1_change = snake
                     y1_change = 0
-                if event.key == K_UP:
+                if event.key == pygame.K_UP:
                     x1_change = 0
                     y1_change = -snake
-                if event.key == K_DOWN:
+                if event.key == pygame.K_DOWN:
                     x1_change = 0
                     y1_change = snake
         if x1 >= win_width or x1<0 or y1>= win_height or y1<0:
@@ -88,7 +87,7 @@ def game_loop():
         x1 += x1_change
         y1 += y1_change
         window.fill(grey)
-        pygame.draw.rect(window,yellow, [foodx,foody,snake,snake])
+        pygame.draw.rect(window,food_color, [foodx,foody,snake,snake])
         snake_size = []
         snake_size.append(x1)
         snake_size.append(y1)
@@ -107,8 +106,8 @@ def game_loop():
 
                 
         if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, win_width - snake)/10.0)*10.0
-            foody = round(random.randrange(0, win_height - snake)/10.0)*10.0
+            foodx = random.randrange(0, win_width - snake, 10)
+            foody = random.randrange(0, win_height - snake, 10)
             snake_length += 1
 
         clock.tick(snake_speed)
